c5c3ad42dc485bbfbf922d7fb42f79fac3384a4d
suleman Ayasrah <SULAIMANALAYASRH@GMAIL.COM>
2025-10-13 20:10:56 +0300

    taskwtsapplite
    .

---
+++ lib/wtsaAppLite.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+class App1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black54,
+          title: Container(
+            color: Colors.black45,
+            margin: EdgeInsets.all(30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 30),
+                  child: Text("Serch Chat"),
+                ),
+
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 30),
+                  child: Icon(Icons.search),
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        body: Container(
+         color: Colors.black54,
+          child: Column(
+            
+            children: [
+           userIcon("Fatimah"),
+          userIcon("Ali"),
+          userIcon("Ahmad"),
+          userIcon("Suleman"),
+          userIcon("Sara"),
+          userIcon("Mohmad"),
+          userIcon("Emad"),
+          userIcon("Omar"),
+          userIcon("Noor"),
+          userIcon("Leen"),
+          
+           
+             
+              
+             
+          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            
+            children: [
+              
+              Container(
+                width: 50,
+                height: 60,
+                margin: EdgeInsets.all(2),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.lightGreen),
+                child: Icon(Icons.add_ic_call)),
+            ],
+          ),
+
+
+          
+          
+          
+          
+          
+          
+            ],
+            
+          
+          ),
+          
+        ),
+        bottomNavigationBar: Container(
+          width: 10,
+          height: 22,
+          child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          
+          children: [
+          
+           Icon(Icons.menu),
+          Icon(Icons.home),
+            Icon(Icons.navigate_before)
+          
+          
+          
+          ],
+          
+          ),
+        ),
+      ),
+
+      debugShowCheckedModeBanner: false,
+    );
+  }
+
+  Column userIcon(s) {
+    return Column(
+      
+      children: [
+        Container(
+
+          
+width: 499,
+
+          color: Colors.black54,
+          margin: EdgeInsets.all(5),
+          
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                
+                children: [
+                  Container(margin: EdgeInsets.all(10), child: Icon(Icons.account_circle,color: Colors.black87),),
+                    Text(s,style: TextStyle(color: Colors.white),)
+
+                ],
+              ),
+             
+
+              Container(child: Column(
+                
+                children: [
+                  Text("10:30 pm",style: TextStyle(color: Colors.white),),
+                  Row(
+                    children: [
+                      Icon(Icons.done,color: Colors.lightGreenAccent,),
+                       Icon(Icons.done,color: Colors.lightGreenAccent,)
+                    ],
+                  )
+                ],
+              )),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
